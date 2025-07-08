a968620246709e5c67a0d58b7bf4a3921b56e826
james.gibbs <james.gibbs@illumo.digital>
2025-07-08 23:00:44 +0100

    middleware on supplier only routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->alias([
+            'supplier' => \App\Http\Middleware\IsSupplier::class,
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
--- routes/web.php
+++ routes/web.php
@@ -27,25 +27,29 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::delete('/products/{product}', [ProductController::class, 'delete'])->name('products.delete');
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 
-Route::post('/cart', [CartController::class, 'addToCart'])->name('cart.add');
-Route::post('/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
+    Route::post('/cart', [CartController::class, 'addToCart'])->name('cart.add');
+    Route::post('/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
 
 
-Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
-Route::post('/orders', [OrderController::class, 'submit'])->name('orders.submit');
+    Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+    Route::post('/orders', [OrderController::class, 'submit'])->name('orders.submit');
+});
+
+Route::middleware(['auth', 'verified','supplier'])->group(function () {
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::delete('/products/{product}', [ProductController::class, 'delete'])->name('products.delete');
+});
 
 //Route::get('/tasks/export', [TaskController::class, 'export'])->name('tasks.export');
 //
 //Route::post('/tasks/bulk-delete', [TaskController::class, 'bulkDelete']);
 //
 //Route::post('/tasks/bulk-complete', [TaskController::class, 'bulkComplete']);
-//});
+
 
 
 require __DIR__.'/auth.php';
