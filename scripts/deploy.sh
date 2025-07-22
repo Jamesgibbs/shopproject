9004e454c40c5c68e2298c0d531d5e0127817d00
james <james@jamess-MacBook-Air.local>
2025-07-22 19:14:19 +0100

    deploy update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,9 @@ ENV_FILE=".env"
 HOST_UID=$(id -u)
 HOST_GID=$(id -g)
 
+export UID=$HOST_UID
+export GID=$HOST_GID
+
 echo "🛠  Starting deployment script"
 echo "    Artifact: $ARTIFACT_PATH"
 echo "    Deploy path: $DEPLOY_PATH"
